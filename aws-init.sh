9b1d0e6aeabd61d49e8d523f551d9dd751ada5ac
Carlos Acevedo <cacevedod@devco.com.co>
2023-02-27 19:31:11 -0500

    scripts de jenkins + sonar y actualizacion del compose en aws

--- aws-init.sh
+++ aws-init.sh
@@ -5,7 +5,8 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 
 # Docker compose install
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+#sudo curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
