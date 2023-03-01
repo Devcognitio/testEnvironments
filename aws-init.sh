e8fad0f5614882f00e31ef8daed1c450eed0647e
Carlos Acevedo <cacevedod@devco.com.co>
2023-02-28 19:34:27 -0500

    quitar lo de sonar

--- aws-init.sh
+++ aws-init.sh
@@ -11,3 +11,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
+
+# Modificacione para correr sonar
+sudo sysctl -w vm.max_map_count=524288
+sudo sysctl -w fs.file-max=131072
